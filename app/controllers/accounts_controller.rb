1f9b854a8c99f7bf54b1c5b78283c7fc6705a09d
lady3bean <annehawleybrett@gmail.com>
2015-02-07 17:34:38 -0500

    adds crud actions to accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,26 @@ class AccountsController < ApplicationController
   def index
     @accounts = Account.all
   end
+
+  def new
+    @account = Account.new(account_params)
+  end
+
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      redirect_to @account
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:name)
+  end
 end
