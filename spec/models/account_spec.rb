feb707a914f112f289d47ae5c39fb056e8bfcf05
lady3bean <annehawleybrett@gmail.com>
2015-02-11 12:51:19 -0500

    passes tests for creating an account

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Account, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Account do
+
+  describe '.create' do
+    it 'creates a new account' do
+      expect(Account.create()).to be_a Account
+    end
+
+    it 'is invalid without a name' do
+      expect(Account.create()).not_to be_valid
+    end
+
+    it 'is valid with a name' do
+      expect(Account.create(name: 'A Name')).to be_valid
+    end
+  end
 end
