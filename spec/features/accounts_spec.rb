3fbda54a86da6d35dc92e26a09e7e82499a642f2
lady3bean <annehawleybrett@gmail.com>
2015-02-11 15:42:37 -0500

    passes feature tests for accounts

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-feature "Accounts", :type => :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Accounts Data Display' do
+  scenario 'list all accounts' do
+
+    Account.create!(name: 'Test Name 1')
+    Account.create!(name: 'Test Name 2')
+    Account.create!(name: 'Test Name 3')
+
+    visit '/accounts'
+
+    expect(page).to have_content 'Accounts'
+    expect(page).to have_content 'Name'
+    expect(page).to have_content 'Balance'
+  end
+
+  scenario 'create an account' do
+    visit '/accounts/new'
+
+    fill_in 'Name', with: 'A Name'
+    click_on 'Create Account'
+
+    expect(page).to have_content(Account.last.name)
+  end
 end
