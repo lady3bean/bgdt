4cc00bb0b4244abcc86e5ab3c57dd2f5059b3d10
lady3bean <annehawleybrett@gmail.com>
2015-02-11 16:48:25 -0500

    passes tests for homepage controller, not sure how to handle testing of creating new entries while on the homepage

--- spec/controllers/home_pages_controller_spec.rb
+++ spec/controllers/home_pages_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe HomePagesController, :type => :controller do
 
+
+  describe 'GET index' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq 200
+    end
+    it 'renders the index template' do
+      get:index
+      expect(response).to render_template('index')
+    end
+    it 'assigns @accounts' do
+      accounts = Account.all
+      get :index
+      expect(assigns(:accounts)).to eq accounts
+    end
+    it 'assigns @categories' do
+      categories = Category.all
+      get :index
+      expect(assigns(:categories)).to eq categories
+    end
+  end
 end
