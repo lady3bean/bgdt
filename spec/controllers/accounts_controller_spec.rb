f8bb45c041c7acb889f3d6a12bf7462eb3925b7d
lady3bean <annehawleybrett@gmail.com>
2015-02-11 14:26:21 -0500

    passes account controller tests for index action

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe AccountsController, :type => :controller do
+RSpec.describe AccountsController do
+  let(:valid_attributes) {
+    { name: 'test name' }
+  }
 
+  let(:invalid_attributes) {
+    { name: nil }
+  }
+
+  describe 'GET index' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the index template' do
+      get:index
+      expect(response).to render_template('index')
+    end
+
+    it 'assigns @accounts' do
+      accounts = Account.all
+      get :index
+      expect(assigns(:accounts)).to eq accounts
+    end
+  end
 end
