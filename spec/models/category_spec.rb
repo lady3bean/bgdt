aa2507abc8995bf42d2df7045819908d50125056
lady3bean <annehawleybrett@gmail.com>
2015-02-11 13:10:34 -0500

    passes tests for creation and validation of categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Category do
+
+  describe '.create' do
+    it 'creates a new category' do
+      expect(Category.create()).to be_a Category
+    end
+
+    it 'is invalid without a cat' do
+      expect(Category.create()).not_to be_valid
+    end
+
+    it 'is valid with a name' do
+      expect(Category.create(cat: 'Test')).to be_valid
+    end
+  end
 end
