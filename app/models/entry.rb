9d2556c75bda8b9b49ec7fab7af6709544627312
lady3bean <annehawleybrett@gmail.com>
2015-02-11 13:06:56 -0500

    passes tests for creation and validation of entries, changes entries model to validate the id's of credit and debit accounts

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,5 +3,5 @@ class Entry < ActiveRecord::Base
   belongs_to :debit_account, class_name: 'Account', foreign_key: 'debit_account_id'
   belongs_to :category
 
-  validates  :amount, :credit_account, :debit_account, presence: true
+  validates  :amount, :credit_account_id, :debit_account_id, presence: true
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Entry, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Entry do
+
+  describe '.create' do
+    it 'creates a new entry' do
+      expect(Entry.create()).to be_a Entry
+    end
+
+    it 'is invalid without an amount' do
+      expect(Entry.create(credit_account_id: 1, debit_account_id: 2)).not_to be_valid
+    end
+
+    it 'is invalid without a credit account' do
+      expect(Entry.create(amount: 50.00, debit_account_id: 1)).not_to be_valid
+    end
+
+    it 'is invalid without a debit account' do
+      expect(Entry.create(amount: 50.00, credit_account_id: 1)).not_to be_valid
+    end
+
+    it 'is valid with and amount, a credit account, and a debit account' do
+      expect(Entry.create(amount: 50.00, credit_account_id: 1, debit_account_id: 1)).to be_valid
+    end
+  end
 end
+
+# validates  :amount, :credit_account, :debit_account, presence: true
