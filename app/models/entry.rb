5159ca9721bba07e0213fe46c8521c8313e7ad54
lady3bean <annehawleybrett@gmail.com>
2015-04-28 15:25:59 -0400

    adds validation and test for entry amounts to be greater than zero

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,9 @@
 class Entry < ActiveRecord::Base
   belongs_to :credit_account, class_name: 'Account', foreign_key: 'credit_account_id'
   belongs_to :debit_account, class_name: 'Account', foreign_key: 'debit_account_id'
+  
   belongs_to :category
 
   validates  :amount, :credit_account_id, :debit_account_id, presence: true
+  validates :amount, :numericality => { :greater_than => 0 }
 end
